60d21eda34b5e583cb3dba1bd9b8845e3fda50f2
Jeremy D. Frens <jfrens@thecitybase.com>
2020-04-10 15:50:56 -0500

    [issue-1] add all_tests alias

--- mix.exs
+++ mix.exs
@@ -3,13 +3,24 @@ defmodule Notris.Umbrella.MixProject do
 
   def project do
     [
+      aliases: aliases(),
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       deps: deps()
+  defp aliases do
+    [
+      all_tests: [
+        "format --check-formatted",
+        "compile --force --warnings-as-errors",
+      ]
     ]
   end
 
   defp deps do
     []
+  defp preferred_cli_env do
+    [
+      all_tests: :test,
+    ]
   end
 end
