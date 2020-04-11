fcf5236d0023ca2c607ba42e539767d27a0f2453
Jeremy D. Frens <jdfrens@gmail.com>
2020-04-11 16:27:03 -0500

    [issue-5] add PropertyTestGenerators.piece/0

--- apps/notris/test/support/property_test_generators.ex
+++ apps/notris/test/support/property_test_generators.ex
@@ -1,23 +1,54 @@
 defmodule Notris.PropertyTestGenerators do
   @moduledoc """
   Generators from property tests.
+
+  NOTE: PropCheck bleeds out an opaque `:proper_types.type()` which makes it difficult to type check this well.  Many
+  entries in `.dialyzer_ignore.exs` are for this file.
   """
 
+  use PropCheck
+
   alias Notris.Piece
-  alias PropCheck.BasicTypes
 
-  @spec rotation :: any()
+  @doc """
+  Generates 0, 1, 2, or 3, the number of right rotations to apply.
+  """
+  @spec rotation :: :proper_types.type()
   def rotation do
-    BasicTypes.choose(0, 3)
+    choose(0, 3)
   end
 
-  @spec shape :: Piece.shape()
+  @doc """
+  Generates a valid shape atom.
+  """
+  @spec shape :: :proper_types.type()
   def shape do
-    Enum.random(~w(i l o t z)a)
+    oneof(~w(i l o t z)a)
   end
 
-  @spec color :: Piece.color()
+  @doc """
+  Generates a valid color.
+  """
+  @spec color :: :proper_types.type()
   def color do
-    Enum.random(~w(red green blue orange)a)
+    oneof(~w(red green blue orange)a)
+  end
+
+  @doc """
+  Generates a boolean: to mirror or not to mirror.
+  """
+  @spec mirror :: :proper_types.type()
+  def mirror do
+    boolean()
+  end
+
+  @doc """
+  Generates a `Notris.Piece.t()`.
+  """
+  @spec piece :: :proper_types.type()
+  def piece do
+    let {shape, mirror, rotate, color} <- {shape(), mirror(), rotation(), color()} do
+      Piece.new(shape, mirror, rotate, color)
+    end
   end
 end
