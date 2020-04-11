617321fec0ca832f8cc078925504daa27cdf64c3
Jeremy D. Frens <jdfrens@gmail.com>
2020-04-11 16:03:13 -0500

    [issue-5] rename PropCheckHelpers to PropertyTestGenerators
    Better to alias as: G than to import

--- apps/notris/test/notris/piece_test.exs
+++ apps/notris/test/notris/piece_test.exs
@@ -1,95 +1,96 @@
 defmodule Notris.PieceTest do
   use ExUnit.Case, async: true
   use PropCheck
-  import Notris.PropCheckHelpers
+
+  alias Notris.PropertyTestGenerators, as: G
 
   alias Notris.Piece
 
   describe "#new/3" do
     property "uses the color" do
-      forall {shape, mirror, rotate, color} <- {shape(), boolean(), rotation(), color()} do
+      forall {shape, mirror, rotate, color} <- {G.shape(), boolean(), G.rotation(), G.color()} do
         match?(%Piece{color: ^color}, Piece.new(shape, mirror, rotate, color))
       end
     end
 
     test "it builds an I" do
-      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {2, 4}]} = Piece.new(:i, false, 0, color())
+      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {2, 4}]} = Piece.new(:i, false, 0, G.color())
     end
 
     test "it builds an L" do
-      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {3, 3}]} = Piece.new(:l, false, 0, color())
+      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {3, 3}]} = Piece.new(:l, false, 0, G.color())
     end
 
     test "it builds an O" do
-      assert %Piece{points: [{1, 1}, {2, 1}, {1, 2}, {2, 2}]} = Piece.new(:o, false, 0, color())
+      assert %Piece{points: [{1, 1}, {2, 1}, {1, 2}, {2, 2}]} = Piece.new(:o, false, 0, G.color())
     end
 
     test "it builds a T" do
-      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {3, 2}]} = Piece.new(:t, false, 0, color())
+      assert %Piece{points: [{2, 1}, {2, 2}, {2, 3}, {3, 2}]} = Piece.new(:t, false, 0, G.color())
     end
 
     test "it builds a Z" do
-      assert %Piece{points: [{1, 1}, {2, 1}, {2, 2}, {3, 2}]} = Piece.new(:z, false, 0, color())
+      assert %Piece{points: [{1, 1}, {2, 1}, {2, 2}, {3, 2}]} = Piece.new(:z, false, 0, G.color())
     end
   end
 
   describe "#rotate_right/1" do
     test "rotates an I" do
-      i = Piece.new(:i, false, 0, color())
+      i = Piece.new(:i, false, 0, G.color())
       assert [{4, 2}, {3, 2}, {2, 2}, {1, 2}] = Piece.rotate_right(i).points
     end
 
     test "rotates an L" do
-      l = Piece.new(:l, false, 0, color())
+      l = Piece.new(:l, false, 0, G.color())
       assert [{3, 2}, {2, 2}, {1, 2}, {1, 3}] = Piece.rotate_right(l).points
     end
 
     test "rotates an O" do
-      o = Piece.new(:o, false, 0, color())
+      o = Piece.new(:o, false, 0, G.color())
       assert [{2, 1}, {2, 2}, {1, 1}, {1, 2}] = Piece.rotate_right(o).points
     end
 
     test "rotates a T" do
-      t = Piece.new(:t, false, 0, color())
+      t = Piece.new(:t, false, 0, G.color())
       assert [{3, 2}, {2, 2}, {1, 2}, {2, 3}] = Piece.rotate_right(t).points
     end
 
     test "rotates a Z" do
-      z = Piece.new(:z, false, 0, color())
+      z = Piece.new(:z, false, 0, G.color())
       assert [{3, 1}, {3, 2}, {2, 2}, {2, 3}] = Piece.rotate_right(z).points
     end
   end
 
   describe "#rotate_left/1" do
     test "rotates an I" do
-      i = Piece.new(:i, false, 0, color())
+      i = Piece.new(:i, false, 0, G.color())
       assert [{1, 3}, {2, 3}, {3, 3}, {4, 3}] = Piece.rotate_left(i).points
     end
 
     test "rotates an L" do
-      l = Piece.new(:l, false, 0, color())
+      l = Piece.new(:l, false, 0, G.color())
       assert [{1, 2}, {2, 2}, {3, 2}, {3, 1}] = Piece.rotate_left(l).points
     end
 
     test "rotates an O" do
-      o = Piece.new(:o, false, 0, color())
+      o = Piece.new(:o, false, 0, G.color())
       assert [{1, 2}, {1, 1}, {2, 2}, {2, 1}] = Piece.rotate_left(o).points
     end
 
     test "rotates a T" do
-      t = Piece.new(:t, false, 0, color())
+      t = Piece.new(:t, false, 0, G.color())
       assert [{1, 2}, {2, 2}, {3, 2}, {2, 1}] = Piece.rotate_left(t).points
     end
 
     test "rotates a Z" do
-      z = Piece.new(:z, false, 0, color())
+      z = Piece.new(:z, false, 0, G.color())
       assert [{1, 3}, {1, 2}, {2, 2}, {2, 1}] = Piece.rotate_left(z).points
     end
   end
 
   describe "#to_glyph" do
     test "I" do
-      piece = Piece.new(:i, false, 0, color())
+      piece = Piece.new(:i, false, 0, G.color())
 
       assert Piece.to_glyph(piece) == """
              _X__
--- apps/notris/test/support/property_test_generators.ex
+++ apps/notris/test/support/property_test_generators.ex
@@ -1,18 +1,22 @@
-defmodule Notris.PropCheckHelpers do
+defmodule Notris.PropertyTestGenerators do
   @moduledoc """
-  Helpers for property tests.
+  Generators from property tests.
   """
 
+  alias Notris.Piece
   alias PropCheck.BasicTypes
 
+  @spec rotation :: any()
   def rotation do
     BasicTypes.choose(0, 3)
   end
 
+  @spec shape :: Piece.shape()
   def shape do
     Enum.random(~w(i l o t z)a)
   end
 
+  @spec color :: Piece.color()
   def color do
     Enum.random(~w(red green blue orange)a)
   end
