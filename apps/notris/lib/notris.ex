be9d184e906631c9c1c4a64bec5a98b7a2f503dc
Jeremy D. Frens <jdfrens@gmail.com>
2020-04-25 16:11:59 -0500

    [issue-17] remove two stupid API functions
    I'll replace them with the needed functions when I get to solving that
    problem.

--- apps/notris/lib/notris.ex
+++ apps/notris/lib/notris.ex
@@ -34,12 +34,6 @@ defmodule Notris do
     game.board.bottom
   end
 
-  @spec rotate_right(Notris.Piece.t()) :: Notris.Piece.t()
-  defdelegate rotate_right(piece), to: Notris.Piece
-
-  @spec rotate_left(Notris.Piece.t()) :: Notris.Piece.t()
-  defdelegate rotate_left(piece), to: Notris.Piece
-
   @spec fill_random_bottom(Board.t()) :: Board.t()
   defp fill_random_bottom(%Board{} = board) do
     new_bottom =
