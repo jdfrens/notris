462f04075e5cba10b2ac4664345f532e7536faef
Jeremy D. Frens <jdfrens@gmail.com>
2020-04-25 17:58:19 -0500

    [issue-19] add Notris.maybe_move_right/1

--- apps/notris/lib/notris.ex
+++ apps/notris/lib/notris.ex
@@ -25,6 +25,9 @@ defmodule Notris do
   @spec maybe_move_left(Game.t()) :: Game.t()
   defdelegate maybe_move_left(game), to: Notris.Game
 
+  @spec maybe_move_right(Game.t()) :: Game.t()
+  defdelegate maybe_move_right(game), to: Notris.Game
+
   @spec maybe_move_down(Game.t()) :: Game.t()
   defdelegate maybe_move_down(game), to: Notris.Game
 
