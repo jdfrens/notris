450d1191107ba3ec2cc43c9223ea5d83cac79381
Jeremy D. Frens <jdfrens@gmail.com>
2020-05-21 16:39:59 -0500

    [issue-21] configure for deployment

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,5 @@ import Config
 config :logger, level: :info
 
 config :notris_web, NotrisWeb.Endpoint,
-  url: [host: "localhost", port: 80],
+  url: [host: "notris.duringrecess.org", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
